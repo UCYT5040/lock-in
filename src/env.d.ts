79bd3877e54c5fa4a2449eb68b0c23bd14e7dee3
Jeremiah Saunders <46662314+UCYT5040@users.noreply.github.com>
2025-11-01 20:47:35 +0000

    Small formatting fixes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,18 +1,20 @@
-import type { Models } from "node-appwrite";
+import type { Models } from 'node-appwrite';
 
 declare global {
-    namespace App {
-        interface Locals {
-            user: {
-                approved: boolean,
-                denied: boolean,
-                lessonDesigner: boolean,
-                admin: boolean,
-                name: string,
-                id: string
-            } | undefined;
-        }
-    }
+	namespace App {
+		interface Locals {
+			user:
+				| {
+						approved: boolean;
+						denied: boolean;
+						lessonDesigner: boolean;
+						admin: boolean;
+						name: string;
+						id: string;
+				  }
+				| undefined;
+		}
+	}
 }
 
 export {};
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -15,67 +15,67 @@ export async function handle({ event, resolve }) {
 		const account = new Account(client);
 		const user = await account.get();
 		event.locals.user = {
-            approved: user.labels.includes('approved'),
-            denied: user.labels.includes('denied'),
+			approved: user.labels.includes('approved'),
+			denied: user.labels.includes('denied'),
 			lessonDesigner: user.labels.includes('lesson-designer'),
 			admin: user.labels.includes('admin'),
 			name: user.name,
 			id: user.$id
 		};
 	} catch (e) {
-        if (!(e instanceof AppwriteException && e.type == 'general_unauthorized_scope')) {
-            console.error('Unexpected error fetching user account:', e);
-        }
+		if (!(e instanceof AppwriteException && e.type == 'general_unauthorized_scope')) {
+			console.error('Unexpected error fetching user account:', e);
+		}
 		event.locals.user = undefined;
 	}
 
-    // For users with the denied label, redirect to '/denied'
-    if (event.url.pathname == '/denied') {
-        if (!event.locals.user?.denied) {
-            return Response.redirect(new URL('/', event.url), 303);
-        }
-    } else {
-        if (event.locals.user?.denied) {
-            return Response.redirect(new URL('/denied', event.url), 303);
-        }
-    }
+	// For users with the denied label, redirect to '/denied'
+	if (event.url.pathname == '/denied') {
+		if (!event.locals.user?.denied) {
+			return Response.redirect(new URL('/', event.url), 303);
+		}
+	} else {
+		if (event.locals.user?.denied) {
+			return Response.redirect(new URL('/denied', event.url), 303);
+		}
+	}
 
-    // For users without the approved label, redirect to '/approval'
-    if (event.url.pathname == '/approval') {
-        if (event.locals.user?.approved) {
-            return Response.redirect(new URL('/', event.url), 303);
-        }
-    } else {
-        if (event.locals.user && !event.locals.user.approved) {
-            return Response.redirect(new URL('/approval', event.url), 303);
-        }
-    }
+	// For users without the approved label, redirect to '/approval'
+	if (event.url.pathname == '/approval') {
+		if (event.locals.user?.approved) {
+			return Response.redirect(new URL('/', event.url), 303);
+		}
+	} else {
+		if (event.locals.user && !event.locals.user.approved) {
+			return Response.redirect(new URL('/approval', event.url), 303);
+		}
+	}
 
-    // Unless the path is in noAuthPaths, require authentication
-    if (!noAuthPaths.includes(event.url.pathname)) {
-        if (!event.locals.user) {
-            return new Response('Unauthorized', { status: 401 });
-        }
-    }
+	// Unless the path is in noAuthPaths, require authentication
+	if (!noAuthPaths.includes(event.url.pathname)) {
+		if (!event.locals.user) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+	}
 
-    // If the user is authenticated and on '/auth', redirect to '/'
-    if (event.url.pathname == '/auth' && event.locals.user) {
-        return Response.redirect(new URL('/', event.url), 303);
-    }
+	// If the user is authenticated and on '/auth', redirect to '/'
+	if (event.url.pathname == '/auth' && event.locals.user) {
+		return Response.redirect(new URL('/', event.url), 303);
+	}
 
-    // If the path is in adminPaths, require admin label
-    if (adminPaths.some((path) => event.url.pathname.startsWith(path))) {
-        if (!event.locals.user?.admin) {
-            return new Response('Forbidden', { status: 403 });
-        }
-    }
+	// If the path is in adminPaths, require admin label
+	if (adminPaths.some((path) => event.url.pathname.startsWith(path))) {
+		if (!event.locals.user?.admin) {
+			return new Response('Forbidden', { status: 403 });
+		}
+	}
 
-    // If the path is in lessonDesignerPaths, require lesson-designer label
-    if (lessonDesignerPaths.some((path) => event.url.pathname.startsWith(path))) {
-        if (!event.locals.user?.lessonDesigner) {
-            return new Response('Forbidden', { status: 403 });
-        }
-    }
+	// If the path is in lessonDesignerPaths, require lesson-designer label
+	if (lessonDesignerPaths.some((path) => event.url.pathname.startsWith(path))) {
+		if (!event.locals.user?.lessonDesigner) {
+			return new Response('Forbidden', { status: 403 });
+		}
+	}
 
-    return resolve(event);
+	return resolve(event);
 }
